44ed7a75ed36e9c71fac4d24fd693e4536e2e37b
jose232628 <jose.utia192326@outlook.es>
2025-03-18 22:31:40 +0100

    database_caragada

---
+++ data/database.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+# Configuración de la conexión
+database = mysql.connector.connect(
+    host = 'localhost',   # O la IP de tu contenedor Docker si usas "docker inspect"
+    port = 3307,
+    ssl_disabled  = True,      # Puerto por defecto de MySQL
+    user = 'root',        # Usuario de MySQL
+    password = 'strong_password',  # La contraseña que configuraste
+    database = 'mysql'    # Puedes cambiarlo al nombre de tu base de datos
+)
